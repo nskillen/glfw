70b61371fb63369b33c6ccea06ab3e0c437c3310
Nathan Skillen <nskillen@gmail.com>
2019-05-19 14:22:44 -0700

    cleaned up premake file a bit more

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 	cdialect "C11"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
 
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "on"
 
 		files {
 			"src/win32_init.c",
@@ -42,3 +42,11 @@ project "GLFW"
 		links {
 			"gdi32.lib"
 		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
